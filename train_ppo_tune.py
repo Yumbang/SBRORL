83b6b3b5886d337f09c09313d194bf32f24f6374
Yumbang <yumbang_ct@icloud.com>
2025-06-23 10:32:56 +0900

    Implemented new scenario generation logic based on objective Q

--- train_ppo_tune.py
+++ train_ppo_tune.py
@@ -2,6 +2,7 @@ import os
 import ray
 from ray import tune
 from ray.rllib.algorithms.ppo import PPOConfig
+from ray.rllib.algorithms.appo import APPOConfig
 from ray.rllib.callbacks.callbacks import RLlibCallback
 import numpy as np
 import datetime as dt
@@ -10,7 +11,7 @@ import polars as pl
 
 # Import your custom environment and the necessary wrappers
 from gymnasium_env.SBROEnvironment import SBROEnv
-from gymnasium_env.utils import sbro_env_creator
+from gymnasium_env.utils import sbro_env_creator, generate_entropy_schedule
 
 
 def main():
@@ -21,9 +22,28 @@ def main():
     num_environments = 24
     # num_environments = 4
     ports = range(8100, 8100 + num_environments)
-    temp_means = np.linspace(15.0, 25.0, num=num_environments)
 
-    SAVE_DIR = os.path.join("./result/PPO", str(dt.datetime.now()))
+    Q_obj = (
+        1.69 / 2
+    )  # Based on FT106 (24-hr moving average: mean=1.69, std=1.52. FT106 contains both aeration product + RO product, thus divide by 2)
+
+    n_time_scenarios = 8
+    n_temp_scenarios = int(np.floor(num_environments/n_time_scenarios))
+
+    if num_environments % n_time_scenarios != 0:
+        print("Number of scenarios does not match with the number of available environments."
+              "Some of the environments are not activated.")
+
+    temp_means = np.linspace(15.0, 18.0, num=n_temp_scenarios)
+    time_means = np.linspace(3.0, 12.0, num=n_time_scenarios)
+    time_highs = time_means * 1.05
+    time_lows = time_means * 0.95
+    V_highs = time_highs * Q_obj
+    V_lows = time_lows * Q_obj
+
+    algorithm = "PPO"
+
+    SAVE_DIR = os.path.join("./result/", algorithm, str(dt.datetime.now()))
     os.makedirs(SAVE_DIR, exist_ok=True)
     os.makedirs(os.path.join(SAVE_DIR, "csvs"))
 
@@ -62,97 +82,156 @@ def main():
 
             episode_dict["Reward"] = rewards
 
-            save_path = os.path.join(
-                SAVE_DIR, "csvs", f"{dt.datetime.now()}.csv"
-            )
+            save_path = os.path.join(SAVE_DIR, "csvs", f"{dt.datetime.now()}.csv")
             episode_df = pl.DataFrame(episode_dict)
             episode_df.write_csv(save_path)
             print(f"Reward: {episode_df['Reward'].sum():.2f}")
-
 
     worker_configs = []
 
     REWARD_CONFIG = {
         "penalty_truncation": 5.0,
         "incentive_termination": 5.0,
-        "penalty_τ": 2.5e-2,
-        "penalty_SEC": (1.0) / 3600.0 / 1000.0,
-        "penalty_conc": 100.0,
+        "penalty_τ": 5.0e-3,
+        "penalty_SEC": (0.25) / 3600.0 / 1000.0,
+        "penalty_conc": 1000.0 / 3600.0,
         "incentive_V_perm": 0.0,
-        "penalty_V_disp": 1.0,
-        "penalty_V_feed": 0.25,
+        "penalty_V_disp": 0.0,
+        "penalty_V_feed": 1.0,
         "penalty_V_perm": 0.01,
+        # "penalty_rapid_change": 0.0,
+        "penalty_rapid_change": 0.05 / 60.0,
     }
 
-    for i in range(num_environments):
-        config = {
-            "base_url": f"http://localhost:{ports[i]}",
-            "scenario_condition": {
-                "T_feed_mean": temp_means[i],
-                "T_feed_std": 0.25,
-                "C_feed_mean": 0.05,
-                "C_feed_std": 0.001,
-            },
-            "objective_condition": {
-                "time_objective_low": 10800.0,
-                "time_objective_high": 18000.0,
-                "V_perm_objective_low": 12.0,
-                "V_perm_objective_high": 16.0,
-            },
-            "reward_conf": REWARD_CONFIG,
-        }
-        worker_configs.append(config)
+    for i in range(len(temp_means)):
+        for j in range(len(time_means)):
+            config = {
+                "base_url": f"http://localhost:{ports[i * len(time_means) + j]}",
+                "scenario_condition": {
+                    "T_feed_mean": temp_means[i],
+                    "T_feed_std": 0.25,
+                    "C_feed_mean": 0.05,
+                    "C_feed_std": 0.001,
+                },
+                "objective_condition": {
+                    "time_objective_low": time_lows[j] * 3600.0,
+                    "time_objective_high": time_highs[j] * 3600.0,
+                    "V_perm_objective_low": V_lows[j],
+                    "V_perm_objective_high": V_highs[j],
+                },
+                "reward_conf": REWARD_CONFIG,
+            }
+            worker_configs.append(config)
 
-    ENV_CONFIG = {"worker_configs": worker_configs, "dt": 30.0}
+    ENV_CONFIG = {"worker_configs": worker_configs, "dt": 60.0}
 
     tune.register_env("sbro_env_v1", sbro_env_creator)
 
-    config = (
-        PPOConfig()
-        .environment(env="sbro_env_v1", env_config=ENV_CONFIG)
-        .env_runners(num_env_runners=num_environments, batch_mode="complete_episodes",gym_env_vectorize_mode="ASYNC")
-        .framework("torch")
-        .resources(
-            # The main Algorithm driver runs on the CPU, so it requires 0 GPUs.
-            num_gpus=0
-        )
-        .learners(
-            # The Learner process gets the GPU.
-            num_learners=1,
-            num_gpus_per_learner=1,
-            num_cpus_per_learner=4
-        )
-        .training(
-            gamma=0.995,
-            lr=5e-5,
-            lambda_=0.95,
-            # Add the model dictionary here to enable the RNN
-            model={
-                # Use a Long Short-Term Memory (LSTM) network.
-                # You could also use "use_gru": True
-                "use_lstm": True,
-                # The number of hidden units in the LSTM layer.
-                # This is the size of the agent's memory.
-                "lstm_cell_size": 256,
-                # The length of the sequence of observations to pass to the RNN for training.
-                # This is a critical hyperparameter to tune.
-                "max_seq_len": 256,
-                # By default, the value function will also share the LSTM encoder.
-                # Set this to False to have a separate feedforward network for the value function.
-                "vf_share_layers": True,
-            },
-            # train_batch_size_per_learner=1024*4
-        )
-        .callbacks(EpisodeReturn)
-    )
+    max_train_step = 1000
 
-    stop_criteria = {"training_iteration": 500}
+    # entropy_coefficient_schedule = generate_entropy_schedule(max_train_step, 0.25, 0.05, 10)
+
+    if algorithm == "PPO":
+        config = (
+            PPOConfig()
+            .environment(env="sbro_env_v1", env_config=ENV_CONFIG)
+            .env_runners(
+                num_env_runners=num_environments,
+                batch_mode="complete_episodes",
+                gym_env_vectorize_mode="SYNC",
+            )
+            .framework("torch")
+            .resources(
+                num_gpus=0
+            )
+            .learners(
+                # The Learner process gets the GPU.
+                num_learners=1,
+                num_gpus_per_learner=1,
+                num_cpus_per_learner=4,
+            )
+            .training(
+                use_critic=True,
+                use_gae=True,
+                gamma=0.995,
+                lr=5e-5,
+                lambda_=0.95,
+                # Add the model dictionary here to enable the RNN
+                model={
+                    # Use a Long Short-Term Memory (LSTM) network.
+                    # You could also use "use_gru": True
+                    "use_lstm": True,
+                    # The number of hidden units in the LSTM layer.
+                    # This is the size of the agent's memory.
+                    "lstm_cell_size": 256,
+                    # The length of the sequence of observations to pass to the RNN for training.
+                    # This is a critical hyperparameter to tune.
+                    "max_seq_len": 256,
+                    # By default, the value function will also share the LSTM encoder.
+                    # Set this to False to have a separate feedforward network for the value function.
+                    "vf_share_layers": True,
+                    "fcnet_hiddens": [32, 64, 128, 256],
+                },
+                entropy_coeff=0.0,
+                # train_batch_size_per_learner=1024*4
+            )
+            .callbacks(EpisodeReturn)
+        )
+    elif algorithm == "APPO":
+        config = (
+            APPOConfig()
+            .environment(env="sbro_env_v1", env_config=ENV_CONFIG)
+            .env_runners(
+                num_env_runners=num_environments,
+                batch_mode="complete_episodes",
+                gym_env_vectorize_mode="ASYNC",
+            )
+            .framework("torch")
+            .resources(
+                # The main Algorithm driver runs on the CPU, so it requires 0 GPUs.
+                num_gpus=0
+            )
+            .learners(
+                # The Learner process gets the GPU.
+                num_learners=1,
+                num_gpus_per_learner=1,
+                num_cpus_per_learner=4,
+            )
+            .training(
+                # use_critic=True,
+                # use_gae=True,
+                gamma=0.995,
+                lr=5e-5,
+                lambda_=0.95,
+                # Add the model dictionary here to enable the RNN
+                model={
+                    # Use a Long Short-Term Memory (LSTM) network.
+                    # You could also use "use_gru": True
+                    "use_lstm": True,
+                    # The number of hidden units in the LSTM layer.
+                    # This is the size of the agent's memory.
+                    "lstm_cell_size": 256,
+                    # The length of the sequence of observations to pass to the RNN for training.
+                    # This is a critical hyperparameter to tune.
+                    "max_seq_len": 256,
+                    # By default, the value function will also share the LSTM encoder.
+                    # Set this to False to have a separate feedforward network for the value function.
+                    "vf_share_layers": True,
+                    "fcnet_hiddens": [32, 64, 128, 256],
+                },
+                # entropy_coeff=entropy_coefficient_schedule,
+                # train_batch_size_per_learner=1024*4
+            )
+            .callbacks(EpisodeReturn)
+        )
+
+    stop_criteria = {"training_iteration": max_train_step}
     storage_path = os.path.abspath(SAVE_DIR)
 
     run_config = tune.RunConfig(
         stop=stop_criteria,
         storage_path=storage_path,
-        name="PPO_SBRO_Custom_Configs_run_FIXED",
+        name=f"{algorithm}_SBRO",
         checkpoint_config=tune.CheckpointConfig(
             checkpoint_score_attribute="env_runners/episode_return_mean",
             checkpoint_score_order="max",
@@ -162,7 +241,7 @@ def main():
     )
 
     tuner = tune.Tuner(
-        "PPO",
+        algorithm,
         param_space=config.to_dict(),
         run_config=run_config,
     )
